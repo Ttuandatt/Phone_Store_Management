d3f239eccf3abcf83172dee7fb9f88b5417b3357
PhuHuynh <134149189+luuphongpro@users.noreply.github.com>
2025-03-23 21:06:18 +0700

    tội đồ của nhóm push lên

---
+++ src/DTO/PBSPDTO.java
@@ -0,0 +1,60 @@
+package DTO;
+
+public class PBSPDTO {
+    private String maPBSP;
+    private String mauSac;
+    private String ram;
+    private String rom;
+    private Integer giaBan;
+    private Integer soLuong;
+    private String trangThai;
+    private String maSP;
+    public String getMaPBSP() {
+        return maPBSP;
+    }
+    public void setMaPBSP(String maPBSP) {
+        this.maPBSP = maPBSP;
+    }
+    public String getMauSac() {
+        return mauSac;
+    }
+    public void setMauSac(String mauSac) {
+        this.mauSac = mauSac;
+    }
+    public String getRam() {
+        return ram;
+    }
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+    public String getRom() {
+        return rom;
+    }
+    public void setRom(String rom) {
+        this.rom = rom;
+    }
+    public Integer getGiaBan() {
+        return giaBan;
+    }
+    public void setGiaBan(Integer giaBan) {
+        this.giaBan = giaBan;
+    }
+    public Integer getSoLuong() {
+        return soLuong;
+    }
+    public void setSoLuong(Integer soLuong) {
+        this.soLuong = soLuong;
+    }
+    public String getTrangThai() {
+        return trangThai;
+    }
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+    public String getMaSP() {
+        return maSP;
+    }
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+}
