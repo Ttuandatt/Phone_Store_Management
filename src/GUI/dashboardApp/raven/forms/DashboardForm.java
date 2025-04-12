c01d30c39dac403571487aa621aecfa0c564b6f8
nguyenmaivy <vynguyen08257@gmail.com>
2025-04-12 21:29:03 +0700

    [update] 12/04/2025 load database to GUI/ThongKeGUI.java, BUS/ThongKeBUS.java, DAO/ThongKeDAO.java, DTO/ThongKeDTO.java, dashboardApp/raven/forms/DashboardForm.java

---
+++ src/GUI/dashboardApp/raven/forms/DashboardForm.java
@@ -0,0 +1,169 @@
+package GUI.dashboardApp.raven.forms;
+
+import BUS.ThongKeBUS;
+import GUI.dashboardApp.components.SimpleForm;
+import com.formdev.flatlaf.FlatClientProperties;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+import raven.chart.ChartLegendRenderer;
+import raven.chart.bar.HorizontalBarChart;
+import raven.chart.line.LineChart;
+import raven.chart.pie.PieChart;
+import GUI.dashboardApp.utils.DateCalculator;
+import raven.chart.data.category.DefaultCategoryDataset;
+import raven.chart.data.pie.DefaultPieDataset;
+
+public class DashboardForm extends SimpleForm {
+
+    public DashboardForm() {
+        thongKeBUS = new ThongKeBUS();
+        init();
+    }
+
+    @Override
+    public void formRefresh() {
+        lineChart.startAnimation();
+        pieChart1.startAnimation();
+        pieChart2.startAnimation();
+        pieChart3.startAnimation();
+        barChart1.startAnimation();
+        barChart2.startAnimation();
+    }
+
+    @Override
+    public void formInitAndOpen() {
+        System.out.println("init and open");
+    }
+
+    @Override
+    public void formOpen() {
+        System.out.println("Open");
+    }
+
+    private void init() {
+        setLayout(new MigLayout("wrap,fill,gap 10", "fill"));
+        createPieChart();
+        createLineChart();
+        createBarChart();
+    }
+
+    private void createPieChart() {
+        pieChart1 = new PieChart();
+        JLabel header1 = new JLabel("Thu thập sản phẩm");
+        header1.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:+1");
+        pieChart1.setHeader(header1);
+        pieChart1.getChartColor().addColor(Color.decode("#f87171"), Color.decode("#fb923c"), Color.decode("#fbbf24"), Color.decode("#a3e635"), Color.decode("#34d399"), Color.decode("#22d3ee"), Color.decode("#818cf8"), Color.decode("#c084fc"));
+        pieChart1.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5,$Component.borderColor,,20");
+        pieChart1.setDataset(thongKeBUS.createProductCollectionData());
+        add(pieChart1, "split 3,height 290");
+
+        pieChart2 = new PieChart();
+        JLabel header2 = new JLabel("Chi phí sản phẩm");
+        header2.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:+1");
+        pieChart2.setHeader(header2);
+        pieChart2.getChartColor().addColor(Color.decode("#f87171"), Color.decode("#fb923c"), Color.decode("#fbbf24"), Color.decode("#a3e635"), Color.decode("#34d399"), Color.decode("#22d3ee"), Color.decode("#818cf8"), Color.decode("#c084fc"));
+        pieChart2.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5,$Component.borderColor,,20");
+        pieChart2.setDataset(thongKeBUS.createProductCostData());
+        add(pieChart2, "height 290");
+
+        pieChart3 = new PieChart();
+        JLabel header3 = new JLabel("Lợi nhuận sản phẩm");
+        header3.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:+1");
+        pieChart3.setHeader(header3);
+        pieChart3.getChartColor().addColor(Color.decode("#f87171"), Color.decode("#fb923c"), Color.decode("#fbbf24"), Color.decode("#a3e635"), Color.decode("#34d399"), Color.decode("#22d3ee"), Color.decode("#818cf8"), Color.decode("#c084fc"));
+        pieChart3.setChartType(PieChart.ChartType.DONUT_CHART);
+        pieChart3.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5,$Component.borderColor,,20");
+        pieChart3.setDataset(thongKeBUS.createProductProfitData());
+        add(pieChart3, "height 290");
+    }
+
+    private void createLineChart() {
+        lineChart = new LineChart();
+        lineChart.setChartType(LineChart.ChartType.CURVE);
+        lineChart.putClientProperty(FlatClientProperties.STYLE, "border:5,5,5,5,$Component.borderColor,,20");
+        lineChart.setCategoryDataset(thongKeBUS.createLineChartData());
+        lineChart.getChartColor().addColor(Color.decode("#38bdf8"), Color.decode("#fb7185"), Color.decode("#34d399"));
+        JLabel header = new JLabel("Dữ liệu thu nhập");
+        header.putClientProperty(FlatClientProperties.STYLE, "font:+1;border:0,0,5,0");
+        lineChart.setHeader(header);
+        // Legend rendering logic
+        SimpleDateFormat df = new SimpleDateFormat("MMM dd, yyyy");
+        try {
+            DefaultCategoryDataset dataset = thongKeBUS.createLineChartData();
+            if (dataset.getColumnCount() > 0) { // Check if dataset has data
+                Date date = df.parse((String) dataset.getColumnKey(0));
+                Date dateEnd = df.parse((String) dataset.getColumnKey(dataset.getColumnCount() - 1));
+                long diff = (dateEnd.getTime() - date.getTime()) / (1000 * 60 * 60 * 24);
+                double d = Math.ceil((diff / 10f));
+                lineChart.setLegendRenderer(new ChartLegendRenderer() {
+                    @Override
+                    public Component getLegendComponent(Object legend, int index) {
+                        if (index % d == 0) {
+                            return super.getLegendComponent(legend, index);
+                        } else {
+                            return null;
+                        }
+                    }
+                });
+            }
+        } catch (ParseException e) {
+            System.err.println("Error parsing dates: " + e.getMessage());
+        }
+        add(lineChart);
+    }
+
+    private void createBarChart() {
+        // BarChart 1
+        barChart1 = new HorizontalBarChart();
+        JLabel header1 = new JLabel("Thu nhập hàng tháng");
+        header1.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:+1;"
+                + "border:0,0,5,0");
+        barChart1.setHeader(header1);
+        barChart1.setBarColor(Color.decode("#f97316"));
+        barChart1.setDataset(thongKeBUS.createMonthlyIncomeData());
+        JPanel panel1 = new JPanel(new BorderLayout());
+        panel1.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5,$Component.borderColor,,20");
+        panel1.add(barChart1);
+        add(panel1, "split 2,gap 0 20");
+
+        // BarChart 2
+        barChart2 = new HorizontalBarChart();
+        JLabel header2 = new JLabel("Chi phí hàng tháng");
+        header2.putClientProperty(FlatClientProperties.STYLE, ""
+                + "font:+1;"
+                + "border:0,0,5,0");
+        barChart2.setHeader(header2);
+        barChart2.setBarColor(Color.decode("#10b981"));
+        barChart2.setDataset(thongKeBUS.createMonthlyCostData());
+        JPanel panel2 = new JPanel(new BorderLayout());
+        panel2.putClientProperty(FlatClientProperties.STYLE, ""
+                + "border:5,5,5,5,$Component.borderColor,,20");
+        panel2.add(barChart2);
+        add(panel2);
+    }
+    private LineChart lineChart;
+    private HorizontalBarChart barChart1;
+    private HorizontalBarChart barChart2;
+    private PieChart pieChart1;
+    private PieChart pieChart2;
+    private PieChart pieChart3;
+    private ThongKeBUS thongKeBUS;
+
+}
