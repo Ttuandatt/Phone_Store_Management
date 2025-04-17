870f433d654dbebb789171d4fabe35e84375425a
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2025-04-17 10:47:20 +0700

    Update JDBCConnection.java

--- src/Database/JDBCConnection.java
+++ src/Database/JDBCConnection.java
@@ -1,4 +1,4 @@
-package Database;
+/*package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -32,6 +32,47 @@ public class JDBCConnection {
 		}
 	}
 
+	// Hàm trả về đối tượng Connection
+	public Connection getConnection() {
+		return con;
+	}
+}
+*/
+
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConnection {
+	private String dbUrl = "jdbc:sqlserver://DAMIAN\\MSSQLSERVER01;databaseName=phonestore;integratedSecurity=true;encrypt=false";
+	private String username = "";
+	private String password = "";
+	private Connection con;
+	
+	//Hàm thiết lập kết nối tới CSDL
+	public boolean openConnection() {
+		boolean result = false;
+		
+		try {
+			con = DriverManager.getConnection(dbUrl, username, password);
+			return true;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//Hàm ngắt kết nối tới CSDL
+	public void closeConnection() {
+		try {
+			if(con!=null)
+				con.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	// Hàm trả về đối tượng Connection
 	public Connection getConnection() {
 		return con;
