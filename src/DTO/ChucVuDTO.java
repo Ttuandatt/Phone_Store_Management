0ef8be987c2380ac8f7381bd41fa28b23320b004
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2025-05-12 22:15:12 +0700

    Update ChucVuDTO.java

--- src/DTO/ChucVuDTO.java
+++ src/DTO/ChucVuDTO.java
@@ -5,46 +5,50 @@ public class ChucVuDTO {
 	private String tenCV;
 	private float luongCoBan;
 	private String trangThai;
-	//Constructor
+
+	// Constructor
 	public ChucVuDTO() {
 	}
-	
-	
-	public ChucVuDTO(String maCV, String tenCV, float heSoLuong, String trangThai) {
+
+	public ChucVuDTO(String maCV, String tenCV, float luongCoBan, String trangThai) {
 		super();
 		this.maCV = maCV;
 		this.tenCV = tenCV;
 		this.luongCoBan = luongCoBan;
-                this.trangThai = trangThai;
+		this.trangThai = trangThai;
 	}
 
-	//getter, setter
+	// getter, setter
 	public String getMaCV() {
 		return maCV;
 	}
+
 	public void setMaCV(String maCV) {
 		this.maCV = maCV;
 	}
+
 	public String getTenCV() {
 		return tenCV;
 	}
+
 	public void setTenCV(String tenCV) {
 		this.tenCV = tenCV;
 	}
+
 	public float getLuongCoBan() {
 		return luongCoBan;
 	}
+
 	public void setLuongCoBan(float luongCoBan) {
 		this.luongCoBan = luongCoBan;
 	}
 
-    public String getTrangThai() {
-        return trangThai;
-    }
+	public String getTrangThai() {
+		return trangThai;
+	}
 
-    public void setTrangThai(String trangThai) {
-        this.trangThai = trangThai;
-    }
-	
+	public void setTrangThai(String trangThai) {
+		this.trangThai = trangThai;
+	}
 
 }
