4e74eeaf9a33aff3cbc3e02ce8141ecad432c124
KhanhHuyen <147577946+jkh199@users.noreply.github.com>
2025-05-12 07:25:37 +0700

    Update BangLuongBUS.java

--- src/BUS/BangLuongBUS.java
+++ src/BUS/BangLuongBUS.java
@@ -1,5 +1,25 @@
 package BUS;
 
+import DAO.BangLuongDAO;
+import DTO.BangLuongDTO;
+import java.util.ArrayList;
+
 public class BangLuongBUS {
+    BangLuongDAO blDAO = new BangLuongDAO();
+    public ArrayList<BangLuongDTO> selectAll() {
+        return blDAO.selectAll();
+    }
+
+    public BangLuongDTO selectById(String maBL) {
+        return blDAO.selectById(maBL);
+    }
+
+    public int updateBangLuong(BangLuongDTO bangLuong) {
+        return blDAO.update(bangLuong);
+    }
+
+    public ArrayList<BangLuongDTO> selectByTime(int thang, int nam) {
+        return blDAO.selectByTime(thang, nam);
+    }
 
 }
