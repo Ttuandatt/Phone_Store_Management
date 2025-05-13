92df5ca5bc423e47527e5a7251cba4f838289e5c
KhanhHuyen <147577946+jkh199@users.noreply.github.com>
2025-05-14 06:53:57 +0700

    Add files via upload

---
+++ src/DTO/LSChinhSuaDTO.java
@@ -0,0 +1,65 @@
+package DTO;
+
+import java.time.LocalDateTime;
+
+
+public class LSChinhSuaDTO {
+	private String maNguoiChinhSua;
+	private String maNguoiBiChinhSua;
+	private LocalDateTime thoiGian;
+	private String giaTriCu;
+        private String giaTriMoi;
+
+    public LSChinhSuaDTO(String maNguoiChinhSua, String maNguoiBiChinhSua, LocalDateTime thoiGian, String giaTriCu, String giaTriMoi) {
+        this.maNguoiChinhSua = maNguoiChinhSua;
+        this.maNguoiBiChinhSua = maNguoiBiChinhSua;
+        this.thoiGian = thoiGian;
+        this.giaTriCu = giaTriCu;
+        this.giaTriMoi = giaTriMoi;
+    }
+
+    public LSChinhSuaDTO() {
+    }
+    
+    public String getMaNguoiChinhSua() {
+        return maNguoiChinhSua;
+    }
+
+    public void setMaNguoiChinhSua(String maNguoiChinhSua) {
+        this.maNguoiChinhSua = maNguoiChinhSua;
+    }
+
+    public String getMaNguoiBiChinhSua() {
+        return maNguoiBiChinhSua;
+    }
+
+    public void setMaNguoiBiChinhSua(String maNguoiBiChinhSua) {
+        this.maNguoiBiChinhSua = maNguoiBiChinhSua;
+    }
+
+    public LocalDateTime getThoiGian() {
+        return thoiGian;
+    }
+
+    public void setThoiGian(LocalDateTime thoiGian) {
+        this.thoiGian = thoiGian;
+    }
+
+    public String getGiaTriMoi() {
+        return giaTriMoi;
+    }
+
+    public void setGiaTriMoi(String giaTriMoi) {
+        this.giaTriMoi = giaTriMoi;
+    }
+
+    public String getGiaTriCu() {
+        return giaTriCu;
+    }
+
+    public void setGiaTriCu(String giaTriCu) {
+        this.giaTriCu = giaTriCu;
+    }
+
+    
+}
