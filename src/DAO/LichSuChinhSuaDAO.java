32d056528cfb4db3e5593f3e6a9805384d9a6614
KhanhHuyen <147577946+jkh199@users.noreply.github.com>
2025-05-14 06:51:51 +0700

    Update LichSuChinhSuaDAO.java

--- src/DAO/LichSuChinhSuaDAO.java
+++ src/DAO/LichSuChinhSuaDAO.java
@@ -1,44 +1,68 @@
 package DAO;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
-import DTO.LichSuChinhSuaDTO;
+import DTO.LSChinhSuaDTO;
 import Database.JDBCConnection;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-public class LichSuChinhSuaDAO implements DAOInterface<LichSuChinhSuaDTO> {
+public class LSChinhSuaDAO implements DAOInterface<LSChinhSuaDTO> {
 	JDBCConnection jdbc = new JDBCConnection();
 
 	@Override
-	public ArrayList<LichSuChinhSuaDTO> selectAll() {
+	public ArrayList<LSChinhSuaDTO> selectAll() {
+            ArrayList<LSChinhSuaDTO> arrLS = new ArrayList<>();
+            try {
+                jdbc.openConnection();
+                String query = "SELECT * FROM LSChinhSua";
+                PreparedStatement ps = jdbc.getConnection().prepareStatement(query);
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    LSChinhSuaDTO ls = new LSChinhSuaDTO();
+                    ls.setMaNguoiBiChinhSua(rs.getString("maNguoiBiChinhSua"));
+                    ls.setThoiGian(rs.getTimestamp("thoiGian").toLocalDateTime()); // nếu kiểu dữ liệu là DATETIME
+                    ls.setMaNguoiChinhSua(rs.getString("maNguoiChinhSua"));
+                    ls.setGiaTriCu(rs.getString("giaTriCu"));
+                    ls.setGiaTriMoi(rs.getString("giaTriMoi"));
+                    arrLS.add(ls);
+                }
+
+                rs.close();
+                ps.close();
+            } catch (Exception e) {
+                e.printStackTrace(); // dùng e.printStackTrace() thay vì chỉ gọi e.getMessage()
+            } finally {
+                jdbc.closeConnection();
+            }
+            return arrLS;
+	}
+
+	@Override
+	public LSChinhSuaDTO selectById(String t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public LichSuChinhSuaDTO selectById(String t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int insert(LichSuChinhSuaDTO lscs) {
+	public int insert(LSChinhSuaDTO lscs) {
 		int result = 0;
 
 		try {
 			jdbc.openConnection();
 
-			String query = "INSERT INTO LSCHINHSUA (maNguoiChinhSua, maNguoiBiChinhSua, thoiGian, noiDungChinhSua) VALUES (?, ?, ?, ?)";
+			String query = "INSERT INTO LSCHINHSUA (maNguoiChinhSua, maNguoiBiChinhSua, thoiGian, giaTriCu, giaTriMoi) VALUES (?, ?, ?, ?, ?)";
 
 			PreparedStatement ps = jdbc.getConnection().prepareStatement(query);
 			ps.setString(1, lscs.getMaNguoiChinhSua());
 			ps.setString(2, lscs.getMaNguoiBiChinhSua());
-			ps.setDate(3, lscs.getThoiGian()); // Đảm bảo thoiGian là kiểu java.sql.Date
-			ps.setNString(4, lscs.getNoiDungChinhSua());
-			
+			ps.setTimestamp(3, Timestamp.valueOf(lscs.getThoiGian()));
+			ps.setNString(4, lscs.getGiaTriCu());
+                        ps.setNString(5, lscs.getGiaTriMoi());	
 			result = ps.executeUpdate();
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			e.getMessage();
@@ -51,15 +75,44 @@ public class LichSuChinhSuaDAO implements DAOInterface<LichSuChinhSuaDTO> {
 	}
 
 	@Override
-	public int delete(LichSuChinhSuaDTO t) {
+	public int delete(LSChinhSuaDTO t) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int update(LichSuChinhSuaDTO t) {
+	public int update(LSChinhSuaDTO t) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+    public ArrayList<LSChinhSuaDTO> selectByMaNV(String maNV) {
+        ArrayList<LSChinhSuaDTO> arrLS = new ArrayList<>();
+        try {
+            jdbc.openConnection();
+            String query = "SELECT * FROM LSChinhSua WHERE maNguoiBiChinhSua = ?";
+            PreparedStatement ps = jdbc.getConnection().prepareStatement(query);
+            ps.setString(1, maNV);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                LSChinhSuaDTO ls = new LSChinhSuaDTO();
+                ls.setMaNguoiBiChinhSua(rs.getString("maNguoiBiChinhSua"));
+                ls.setThoiGian(rs.getTimestamp("thoiGian").toLocalDateTime());
+                ls.setMaNguoiChinhSua(rs.getString("maNguoiChinhSua"));
+                ls.setGiaTriCu(rs.getString("giaTriCu"));
+                ls.setGiaTriMoi(rs.getString("giaTriMoi"));
+                arrLS.add(ls);
+            }
+
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace(); // dùng e.printStackTrace() thay vì chỉ gọi e.getMessage()
+        } finally {
+            jdbc.closeConnection();
+        }
+        return arrLS;
+    }
 
 }
